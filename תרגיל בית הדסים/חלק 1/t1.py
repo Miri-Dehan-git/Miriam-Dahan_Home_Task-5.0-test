8de76954c2054b7b81cbe67457db7e596260fb3d
Miri-Dehan-git <38328179932@mby.co.il>
2025-05-05 01:12:34 +0300

    Update t1.py

--- תרגיל בית הדסים/חלק 1/t1.py
+++ תרגיל בית הדסים/חלק 1/t1.py
@@ -1,26 +1,38 @@
 import heapq
 from collections import defaultdict
+from itertools import islice
 
 def process_text_log_file(file_path, top_n, chunk_size=100000):
-    error_counts = defaultdict(int)
+    total_error_counts = defaultdict(int)
+
     with open(file_path, 'r', encoding='utf-8') as f:
-        for line in f:
-            line = line.strip()
-            if "Error:" in line:
-                error_code = line.split("Error:")[1]
-                error_counts[error_code] += 1
+        while True:
+            lines = list(islice(f, chunk_size))
+            if not lines:
+                break
 
-    top_errors = heapq.nlargest(top_n, error_counts.items(), key=lambda x: x[1])
+            chunk_error_counts = defaultdict(int)
 
+            for line in lines:
+                line = line.strip()
+                if "Error:" in line:
+                    error_code = line.split("Error:")[1].strip()
+                    chunk_error_counts[error_code] += 1
+
+            for code, count in chunk_error_counts.items():
+                total_error_counts[code] += count
+
+
+    top_errors = heapq.nlargest(top_n, total_error_counts.items(), key=lambda x: x[1])
     return top_errors
 
-path = r"C:\Users\This User\Documents\תרגיל בית הדסים\קבצים שנשלחו\log.txt"
+path = "C:\\Users\\This User\\Documents\\Miriam-Dahan_Home_Task-5.0-test\\תרגיל בית הדסים\\קבצים שנשלחו\\log.txt"
+top_errors = process_text_log_file(path, top_n=5, chunk_size=50000)
 
-top_errors = process_text_log_file(path, 5)
-
-print("Nקודי השגיאה השכיחים ביותר:\n")
+print("\nקודי השגיאה השכיחים ביותר בכל הקובץ:\n")
 for error_code, count in top_errors:
     print(f"{error_code}: {count}")
+
 
 #ניתוח זמן ריצה:
 #O(L + U * log N)-כאשר:U = מספר קודי שגיאה שונים,N = מספר השגיאות הכי נפוצות שצריך להחזיר,L=מספר שורות בקובץ
